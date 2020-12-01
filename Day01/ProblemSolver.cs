f9a2f56621bf78fb55e9e3e8499fa3ba343884fb
Calvin Reed <calvinreed@posteo.net>
2020-12-01 10:19:11 -0500

    Init

---
+++ Day01/ProblemSolver.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day01
+{
+    public class ProblemSolver
+    {
+        private readonly IReadOnlySet<int> inputSet;
+        private readonly ReadOnlyMemory<int> inputMem;
+
+        public ProblemSolver(IEnumerable<int> input)
+        {
+            inputSet = input.ToHashSet();
+            inputMem = inputSet.ToArray();
+        }
+
+        public int? ComputeTriple(int total)
+        {
+            for (var i = 0; i < inputMem.Length; i++)
+            {
+                var n = inputMem.Span[i];
+                if (ComputeCouple(total - n, inputMem.Span[i..]) is { } product)
+                {
+                    return n * product;
+                }
+            }
+
+            return null;
+        }
+
+        public int? ComputeCouple(int total)
+        {
+            return ComputeCouple(total, inputMem.Span);
+        }
+
+        private int? ComputeCouple(int total, ReadOnlySpan<int> input)
+        {
+            foreach (var n in input)
+            {
+                var diff = total - n;
+                if (inputSet.Contains(diff))
+                {
+                    return n * diff;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Day01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day01
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var solver = new ProblemSolver(ReadInput(args[0]));
+            Console.WriteLine(solver.ComputeCouple(2020));
+            Console.WriteLine(solver.ComputeTriple(2020));
+        }
+
+        private static IEnumerable<int> ReadInput(string path)
+        {
+            return File.ReadLines(path).Select(int.Parse);
+        }
+    }
+}
