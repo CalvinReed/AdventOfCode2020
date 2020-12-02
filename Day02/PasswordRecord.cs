e67f919b32e5bca919a44ae0508be39a5e60d163
Calvin Reed <calvinreed@posteo.net>
2020-12-02 08:36:59 -0500

    Modified parsing and validity check. Solved part 2.

--- Day02/PasswordRecord.cs
+++ Day02/PasswordRecord.cs
@@ -1,22 +1,14 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Day02
 {
-    public record PasswordRecord(int MinCount, int MaxCount, char CheckChar, string Password)
+    public record PasswordRecord(int FirstIndex, int SecondIndex, char CheckChar, string Password)
     {
-        [SuppressMessage("ReSharper", "MemberCanBeMadeStatic.Global")] // Rider 2020.3 EAP9 erroneously suggests that this property be made static.
-        public bool IsValid
-        {
-            get
-            {
-                var checkChar = CheckChar;
-                var checkCount = Password.Count(x => x == checkChar); // Rider 2020.3 EAP9 shows error when capturing "CheckChar" in lambda, despite being valid code to the compiler.
-                return checkCount >= MinCount && checkCount <= MaxCount;
-            }
-        }
+        // Rider 2020.3 EAP9 erroneously suggests that this property be made static
+        [SuppressMessage("ReSharper", "MemberCanBeMadeStatic.Global")]
+        public bool IsValid => Password[FirstIndex] == CheckChar != (Password[SecondIndex] == CheckChar);
 
         public static PasswordRecord Parse(string str)
         {
@@ -26,10 +18,10 @@ namespace Day02
                 throw new ArgumentException(null, nameof(str));
             }
 
-            var min = int.Parse(match.Groups[1].Value);
-            var max = int.Parse(match.Groups[2].Value);
-            var check = match.Groups[3].Value[0];
-            return new PasswordRecord(min, max, check, match.Groups[4].Value);
+            var firstIndex = int.Parse(match.Groups[1].Value) - 1;
+            var secondIndex = int.Parse(match.Groups[2].Value) - 1;
+            var checkChar = match.Groups[3].Value[0];
+            return new PasswordRecord(firstIndex, secondIndex, checkChar, match.Groups[4].Value);
         }
     }
 }
