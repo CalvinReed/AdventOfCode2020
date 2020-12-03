e36d3ac4a2eea9fb4a980d3c9ba48cd1cfdd66e9
Calvin Reed <calvinreed@posteo.net>
2020-12-03 06:53:22 -0500

    Created project for day 3

---
+++ Day03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace Day03
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (var line in File.ReadLines(args[0]))
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
