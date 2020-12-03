35567399077bc7b42ad1964c1ac7df60471f98b8
Calvin Reed <calvinreed@posteo.net>
2020-12-03 07:10:06 -0500

    Implemented descent iterator. Part 1 complete.

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Day03
 {
@@ -7,9 +9,35 @@ namespace Day03
     {
         private static void Main(string[] args)
         {
-            foreach (var line in File.ReadLines(args[0]))
+            var map = ReadMap(args[0]);
+            var treeCount = map
+                .Descend(3, 1)
+                .Count(x => x == '#');
+            Console.WriteLine(treeCount);
+        }
+
+        private static char[,] ReadMap(string path)
+        {
+            var lines = File.ReadAllLines(path);
+            var map = new char[lines[0].Length, lines.Length];
+            for (var y = 0; y < lines.Length; y++)
             {
-                Console.WriteLine(line);
+                for (var x = 0; x < lines[y].Length; x++)
+                {
+                    map[x, y] = lines[y][x];
+                }
+            }
+
+            return map;
+        }
+
+        private static IEnumerable<char> Descend(this char[,] map, int run, int rise)
+        {
+            var x = 0;
+            for (var y = 0; y < map.GetLength(1); y += rise)
+            {
+                yield return map[x, y];
+                x = (x + run) % map.GetLength(0);
             }
         }
     }
