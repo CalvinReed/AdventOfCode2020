5858071ef5c03bd487be68cd22bf64548d7a07c1
Calvin Reed <calvinreed@posteo.net>
2020-12-04 08:00:40 -0500

    Completed part 2

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Day04
 {
@@ -11,6 +12,56 @@ namespace Day04
         {
             var validCount = ReadData(args[0]).Count(IsValid);
             Console.WriteLine(validCount);
+        }
+
+        private static bool IsValid(IReadOnlyDictionary<string, string> passport)
+        {
+            var isValid =
+                passport.RangeCheck("byr", 1920, 2002) &&
+                passport.RangeCheck("iyr", 2010, 2020) &&
+                passport.RangeCheck("eyr", 2020, 2030) &&
+                passport.HeightCheck() &&
+                passport.MatchCheck("hcl", @"^#[0-9a-f]{6}$") &&
+                passport.MatchCheck("ecl", @"^(?:amb|blu|brn|gry|grn|hzl|oth)$") &&
+                passport.MatchCheck("pid", @"^\d{9}$");
+            return isValid;
+        }
+
+        private static bool RangeCheck(this IReadOnlyDictionary<string, string> passport, string key, int min, int max)
+        {
+            var isValid =
+                passport.TryGetValue(key, out var value)
+                && int.TryParse(value, out var n)
+                && n >= min
+                && n <= max;
+            return isValid;
+        }
+
+        private static bool MatchCheck(this IReadOnlyDictionary<string, string> passport, string key, string pattern)
+        {
+            return passport.TryGetValue(key, out var value) && Regex.IsMatch(value, pattern);
+        }
+
+        private static bool HeightCheck(this IReadOnlyDictionary<string, string> passport)
+        {
+            if (!passport.TryGetValue("hgt", out var value))
+            {
+                return false;
+            }
+
+            var match = Regex.Match(value, @"^(\d{2,3})(cm|in)$");
+            if (!match.Success)
+            {
+                return false;
+            }
+
+            var n = int.Parse(match.Groups[1].Value);
+            return match.Groups[2].Value switch
+            {
+                "cm" => n >= 150 && n <= 193,
+                "in" => n >= 59 && n <= 76,
+                _ => throw new Exception("UNREACHABLE")
+            };
         }
 
         private static IEnumerable<Dictionary<string, string>> ReadData(string path)
@@ -32,19 +83,6 @@ namespace Day04
             {
                 yield return ToPassport(buffer);
             }
-        }
-
-        private static bool IsValid(IReadOnlyDictionary<string, string> passport)
-        {
-            var isValid =
-                passport.ContainsKey("byr") &&
-                passport.ContainsKey("iyr") &&
-                passport.ContainsKey("eyr") &&
-                passport.ContainsKey("hgt") &&
-                passport.ContainsKey("hcl") &&
-                passport.ContainsKey("ecl") &&
-                passport.ContainsKey("pid");
-            return isValid;
         }
 
         private static Dictionary<string, string> ToPassport(IEnumerable<string> data)
