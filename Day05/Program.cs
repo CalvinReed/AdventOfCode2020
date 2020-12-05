07f98ddf3a85f42fd0192156db0de26dbe999c3e
Calvin Reed <calvinreed@posteo.net>
2020-12-05 05:25:28 -0500

    Created project. Wrote parsing method for boarding pass. Completed part 1.

---
+++ Day05/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day05
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var maxId = File.ReadLines(args[0])
+                .Select(BoardingPass.Parse)
+                .Max(x => x.Id);
+            Console.WriteLine(maxId);
+        }
+    }
+}
