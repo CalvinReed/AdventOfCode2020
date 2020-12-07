4b8e84b6bff1dc7f648ea1d92c923d01c68299ed
Calvin Reed <calvinreed@posteo.net>
2020-12-07 13:32:49 -0500

    Completed day 7, part 2. Pulled part 1 code out into new method.

--- AoC2020/Day07/Program.cs
+++ AoC2020/Day07/Program.cs
@@ -11,23 +11,43 @@ namespace AoC2020.Day07
         {
             var rules = File.ReadLines(path)
                 .Select(BagRule.Parse)
-                .ToList();
-            var inverted = Invert(rules);
-            var topLevel = new HashSet<string>();
-            var queue = new Queue<string>(inverted["shiny gold"]);
-            while (queue.TryDequeue(out var color))
+                .ToDictionary(x => x.Color);
+            var containers = PossibleContainers(rules.Values, "shiny gold");
+            var capacity = CalculateCapacity(rules, "shiny gold");
+            Console.WriteLine(containers.Count);
+            Console.WriteLine(capacity);
+        }
+
+        private static int CalculateCapacity(IReadOnlyDictionary<string, BagRule> lookup, string color)
+        {
+            int Capacity(KeyValuePair<string, int> pair)
             {
-                topLevel.Add(color);
-                foreach (var nextColor in inverted[color].Where(x => !topLevel.Contains(x)))
+                var (key, value) = pair;
+                return value + value * CalculateCapacity(lookup, key);
+            }
+
+            var rule = lookup[color];
+            return rule.Contents.Sum(Capacity);
+        }
+
+        private static HashSet<string> PossibleContainers(IReadOnlyCollection<BagRule> rules, string color)
+        {
+            var lookup = InvertedLookup(rules);
+            var topLevel = new HashSet<string>();
+            var queue = new Queue<string>(lookup[color]);
+            while (queue.TryDequeue(out var currentColor))
+            {
+                topLevel.Add(currentColor);
+                foreach (var nextColor in lookup[currentColor].Where(x => !topLevel.Contains(x)))
                 {
                     queue.Enqueue(nextColor);
                 }
             }
 
-            Console.WriteLine(topLevel.Count);
+            return topLevel;
         }
 
-        private static Dictionary<string, HashSet<string>> Invert(IReadOnlyCollection<BagRule> rules)
+        private static Dictionary<string, HashSet<string>> InvertedLookup(IReadOnlyCollection<BagRule> rules)
         {
             var revLookup = new Dictionary<string, HashSet<string>>(rules.Count);
             foreach (var rule in rules)
